8893bbe9a7c5c43da1b14558a8d9861329bbad75
Dan Vanderkam <danvdk@gmail.com>
2025-06-08 12:47:04 -0400

    small test

--- test/sumbound_test.py
+++ test/sumbound_test.py
@@ -2,6 +2,22 @@ from listings.sumbound import set_size, sum_bound
 from .util import get_enable2k_trie
 
 
+def test_sumbound_fur_far():
+  t = get_enable2k_trie()
+  t.reset_marks()
+  set_size(4)
+  # fur, far, arf
+  assert 3 == sum_bound(
+    [
+      "f",
+      "au",
+      ".",
+      "r",
+    ],
+    t,
+  )
+
+
 def test_sumbound_big():
   t = get_enable2k_trie()
   t.reset_marks()
